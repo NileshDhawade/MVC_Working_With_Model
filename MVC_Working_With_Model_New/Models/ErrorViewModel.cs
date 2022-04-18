42a7d3739ee67a85bd7cebe029fc195351b1e2cc
HP <HP@DESKTOP-QJNNUMD>
2022-04-18 09:37:32 +0530

    Add project files.

---
+++ MVC_Working_With_Model_New/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Working_With_Model_New.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
